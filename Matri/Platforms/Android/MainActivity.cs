b1dda8cc4fae163563b69b407d55b56042b94e46
sham_abraham <ganikapudi.sham@gmail.com>
2024-04-27 20:28:35 +0530

    Added close button to Entry controls.

--- Matri/Platforms/Android/MainActivity.cs
+++ Matri/Platforms/Android/MainActivity.cs
@@ -13,7 +13,7 @@ namespace Matri
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
-        internal static readonly string Channel_ID = "TestChannel";
+        internal static readonly string Channel_ID = "ChristianJodiNC";
         internal static readonly int NotificationID = 101;
         protected override void OnCreate(Bundle savedInstanceState)
         {
