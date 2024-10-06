4a88694509698abffe95f9672698cf10189386e2
sham_abraham <ganikapudi.sham@gmail.com>
2024-10-06 10:47:49 +0530

    Profile photo resized

--- Matri/App.xaml.cs
+++ Matri/App.xaml.cs
@@ -7,7 +7,7 @@ namespace Matri
         public static string BaseImageUrl { get; } = "https://cdn.syncfusion.com/essential-ui-kit-for-xamarin.forms/common/uikitimages/";
         public App(AppShellViewModel viewModel)
         {
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NBaF5cXmZCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWXxcc3VURmBdVEd2XUc=");
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NCaF5cXmZCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWXlcdnZWRGdZU01yWUs=");
             InitializeComponent();
 
             MainPage = new AppShell(viewModel);
