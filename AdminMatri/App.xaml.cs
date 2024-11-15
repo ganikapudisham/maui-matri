b7b449e28e03ac773154891e6372bf5064649738
sham_abraham <ganikapudi.sham@gmail.com>
2024-11-15 12:36:30 +0530

    Button shadow Added Admin Matri

---
+++ AdminMatri/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AdminMatri
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AdminMatri/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AdminMatri
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
