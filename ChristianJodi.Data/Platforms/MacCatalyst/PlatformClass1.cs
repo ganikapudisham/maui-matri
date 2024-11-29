68f400d464ac608a82c895baf75aec16e73ead4c
sham_abraham <ganikapudi.sham@gmail.com>
2024-11-29 18:03:20 +0530

    App not working in release mode reasons 1)Removed FontFamily="Montserrat-Medium" 2)Removed StaticResource FormBorderStyle

---
+++ ChristianJodi.Data/Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace ChristianJodi.Data
+{
+    // All the code in this file is only included on Mac Catalyst.
+    public class PlatformClass1
+    {
+    }
+}
---
+++ ChristianJodi.Data/Platforms/iOS/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace ChristianJodi.Data
+{
+    // All the code in this file is only included on iOS.
+    public class PlatformClass1
+    {
+    }
+}
